461b57363e4fa69e00157dd0fe3e1671e65eb47a
Ivan Tsyganok <tsyganok.ivan@gmail.com>
2018-07-14 22:18:44 +0300

    create order POST request, move file if success, basic archive view

--- routes/helper.js
+++ routes/helper.js
@@ -32,6 +32,45 @@ module.exports = buyerData = (bayerGln) => {
     }
 };
 
+module.exports = shipToData = (shipToGln) => {
+    var shipToMap = [
+        ["4829900023805", 6011555, "Kyiv, Frunze"],
+        ["4829900023829", 6011748, "Kyiv, Yaroslavska str.28"],
+        ["4829900024802", 6012878, "Kyiv, Petrovka"],
+        ["4829900023806", 6011124, "Kyiv, Kozatska str. 120/4"],
+        ["4829900023807", 6011142, "Kyiv, Vasilkivska str.1"]
+    ];
+
+    for (var i = 0; i < shipToMap.length; i++) {
+        for(var j = 0; j < shipToMap[i].length; j++) {
+            if (shipToGln == shipToMap[i][j]) {
+                return shipToMap[i];
+            }
+        }
+    }
+};
+
+module.exports = jdeDate = (ediDate) => {
+    var myDate = new Date(ediDate);
+    var start = new Date(myDate.getFullYear(), 0, 0);
+    var diff = (myDate - start) + ((start.getTimezoneOffset() - myDate.getTimezoneOffset()) * 60 * 1000);
+    var dayInYear = Math.floor(diff / (1000 * 60 * 60 * 24));
+    var orderDate = '1'+ myDate.getFullYear().toString().substr(-2) + dayInYear;
+
+    return orderDate;
+};
+
+module.exports = moveFile = (file, dir2, newName) => {
+
+    var f = path.basename(file);
+    var dest = path.resolve(dir2, newName);
+
+    fs.rename(file, dest, (err) => {
+        if(err) throw err;
+        else console.log('Successfully moved');
+    });
+};
+
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express       = require('express');
 var router        = express.Router();
-var helper        = require('./helper');
 
 var xml2js        = require('xml2js');
 var parser        = new xml2js.Parser();
+var path          = require('path');
+var request       = require('request');
+
+var helper        = require('./helper');
+
 var sourceFolder  = '/Users/ivantsyganok/Documents/NodeJS/EDI/public/xmlfiles/2/';
+var archiveFolder = '/Users/ivantsyganok/Documents/NodeJS/EDI/public/xmlfiles/archive/';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -48,6 +53,126 @@ router.get('/details/:id', function(req, res, next) {
                     res.render('details', { orderId:  req.params.id, result: order['ORDER']['HEAD'][0]['POSITION'] });
                 }
             })
+
+        }, error => console.log(error));
+
+});
+
+router.get('/createOrder/:id', function(req, res, next) {
+
+    readFiles(sourceFolder)
+        .then( allContents => {
+
+            var results = [];
+
+            allContents.forEach(function (item) {
+                parser.parseString(item[1], function (err, result) {
+                    results.push(Object.assign(result, {"FILENAME": item[0]}));
+                })
+            })
+
+            results.forEach(function (order) {
+                if (order['ORDER'].NUMBER == req.params.id ) {
+
+                    var orderDate = jdeDate(order['ORDER'].DATE);
+                    var deliveryPlaseGln = order['ORDER']['HEAD'][0]['DELIVERYPLACE'][0];
+                    var buyerGln = order['ORDER']['HEAD'][0]['BUYER'][0];
+                    var login = buyerData(buyerGln)[2];
+                    var pass = buyerData(buyerGln)[3];
+                    var shipTo = shipToData(deliveryPlaseGln)[1];
+                    var fileName = order['FILENAME'];
+                    console.log(fileName);
+
+                    var orderLines = [];
+
+                    order['ORDER']['HEAD'][0]['POSITION'].forEach(function (orderLine) {
+                        var jsonOrderLine = {
+                            "productId": parseInt(orderLine.PRODUCTIDSUPPLIER[0]),
+                            "quantity": parseInt(orderLine.ORDEREDQUANTITY[0]),
+                            "price": parseFloat(orderLine.PRICEWITHVAT[0]),
+                            "customerProductId": parseInt(orderLine.PRODUCTIDBUYER[0])
+                        };
+                        orderLines.push(jsonOrderLine);
+                    });
+
+                    var json = {
+                        "deliveryAddress": parseInt(shipTo),
+                        "requestedDeliveryDate": parseInt(orderDate),
+                        "orderIfAllAvailable": false,
+                        "deliveryInstructions": "Номер заказа: " + order['ORDER'].NUMBER + ", " +
+                        "точка доставки:" + deliveryPlaseGln,
+                        "customerPO": order['ORDER'].NUMBER[0],
+                        "orderLines": orderLines
+                    };
+
+                    console.log(json);
+
+                    var options = {
+                        url: 'https://kiev.elkogroup.com/api/Sales/CreateOrder?username=' + login + '&password=' + pass,
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        json: json
+                    };
+
+                    request(options, function(err, result, body) {
+                        if (result && (result.statusCode === 200 || result.statusCode === 201)) {
+                            console.log(result.body);
+                            moveFile('../EDI/public/xmlfiles/2/' + fileName, '../EDI/public/xmlfiles/archive/',
+                                'SP_' + result.body.orderId
+                                + '_' + buyerData(buyerGln)[1]
+                                + '_' + result.body.dateCreate +'.xml');
+
+                            var content = JSON.stringify(result.body);
+                            var jsonFileName = '../EDI/public/xmlfiles/archive/'
+                                + 'SP_' + result.body.orderId
+                                + '_' + buyerData(buyerGln)[1]
+                                + '_' + result.body.dateCreate +'.json'
+
+                            fs.writeFile(jsonFileName, content, 'utf8', function (err) {
+                                if (err) {
+                                    return console.log(err);
+                                }
+
+                                console.log("The file was saved!");
+                            });
+
+                        } else {
+                            console.log(result.statusCode + ' - ' + result.statusMessage);
+                            console.log(body);
+                        }
+
+                        res.render('createOrder', {
+                            orderId:  req.params.id,
+                            statusCode: result.statusCode,
+                            statusMessage: result.statusMessage,
+                            result: result.body
+                        });
+                    });
+
+                }
+            })
+
+        }, error => console.log(error));
+});
+
+router.get('/archive', function(req, res, next) {
+
+    readFiles(archiveFolder)
+        .then( allContents => {
+
+            var results = [];
+
+            allContents.forEach(function (item) {
+                if (path.extname(item[0]) == '.xml') {
+                    parser.parseString(item[1], function (err, result) {
+                        results.push(result);
+                    })
+                }
+            })
+
+            res.render('archive', { result:  results });
 
         }, error => console.log(error));
 
